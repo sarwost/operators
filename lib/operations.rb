329095156c3cbe8d283b02dcb7a09153f531bd21
sarwost <swostenberg@gmail.com>
2021-07-01 16:23:04 -0600

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+    if speed.between?(40, 60)
+        false
+    else 
+        true
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed.between?(40, 60) ? false : true
 end
 	
 
